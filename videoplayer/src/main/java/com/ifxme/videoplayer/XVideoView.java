fd9bc2a292dbc72b02682f8b5d921f933456b567
Slbw <teng1234567ha@126.com>
2018-10-22 22:29:55 +0800

    初始提交

---
+++ videoplayer/src/main/java/com/ifxme/videoplayer/XVideoView.java
@@ -0,0 +1,24 @@
+package com.ifxme.videoplayer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+/**
+ * @auth Slbw
+ * @date 2018/10/22 22:26
+ **/
+public class XVideoView extends TextureView {
+
+    public XVideoView(Context context) {
+        super(context);
+    }
+
+    public XVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
